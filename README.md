2ddba492059554dbbbde3ca78070c515d5805fb8
FrenkenFlores <fflores@student.21-school.ru>
2021-12-05 12:07:26 +0300

    2 bcs: add button

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Задание участникам трэка Android
+
+
+
+
+
+## Sources
+- [ISS MOEX](https://iss.moex.com/iss/reference/)
+- [ISS MOEX interface](https://www.moex.com/a2193)
+- [Habr ISS MOEX](https://habr.com/ru/post/486716/)
--- app/src/main/java/com/codenrock/bcsgetlab/evloev/obligationcalculator/MainActivity.java
+++ app/src/main/java/com/codenrock/bcsgetlab/evloev/obligationcalculator/MainActivity.java
@@ -3,12 +3,24 @@ package com.codenrock.bcsgetlab.evloev.obligationcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button button_create_portfolio;
+    private final String TAG_C_P="button_create_portfolio";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_create_portfolio = (Button) findViewById(R.id.button_create_portfolio);
+        button_create_portfolio.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d(TAG_C_P, "Button");
     }
 }
