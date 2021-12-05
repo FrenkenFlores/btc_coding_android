56c21cf08111a4bb938a49003ad94a5869862321
FrenkenFlores <fflores@student.21-school.ru>
2021-12-05 12:52:54 +0300

    3 bcs: set connection

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - [ISS MOEX](https://iss.moex.com/iss/reference/)
 - [ISS MOEX interface](https://www.moex.com/a2193)
 - [Habr ISS MOEX](https://habr.com/ru/post/486716/)
+- [Java.net for Android](http://developer.alexanderklimov.ru/android/java/net.php)
--- app/src/main/java/com/codenrock/bcsgetlab/evloev/obligationcalculator/MainActivity.java
+++ app/src/main/java/com/codenrock/bcsgetlab/evloev/obligationcalculator/MainActivity.java
@@ -2,25 +2,72 @@ package com.codenrock.bcsgetlab.evloev.obligationcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private URL url;
+    private HttpURLConnection connection;
     private Button button_create_portfolio;
     private final String TAG_C_P="button_create_portfolio";
+    private final String TAG_URL="set_connection";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         button_create_portfolio = (Button) findViewById(R.id.button_create_portfolio);
         button_create_portfolio.setOnClickListener(this);
     }
 
+    class SetHttpConnection extends AsyncTask<String, Integer, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                url = new URL(getResources().getString(R.string.url));
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setConnectTimeout(3000);
+                connection.setReadTimeout(3000);
+                if (connection.getResponseCode() == 200) {
+                    return connection.getResponseCode() + ": connection failed";
+                }
+            } catch (MalformedURLException e) {
+                Log.e(TAG_URL, e.getMessage());
+                e.printStackTrace();
+                return "URL: connection failed";
+            } catch (IOException e) {
+                Log.e(TAG_URL, e.getMessage());
+                e.printStackTrace();
+                return "openConnection: connection failed";
+            }
+            return "connection succeeded";
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
     @Override
     public void onClick(View v) {
         Log.d(TAG_C_P, "Button");
+        Log.d(TAG_URL, getResources().getString(R.string.url));
     }
 }
